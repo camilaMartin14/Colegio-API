2036b079c39f84e6108be0ce0cdb87c7163ae2fc
camilaMartin14 <camilamartin1403@gmail.com>
2024-08-02 08:01:15 -0300

    subir

---
+++ src/main/java/com/colegio/colegio_api/ColegioApiApplication.java
@@ -0,0 +1,13 @@
+package com.colegio.colegio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColegioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColegioApiApplication.class, args);
+	}
+
+}
